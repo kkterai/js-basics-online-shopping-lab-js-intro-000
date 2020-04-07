d63af67a90d4d393f6d5a469232e68f0d187f518
Nick VanderMolen <tree.vandermolen@gmail.com>
2020-04-07 00:22:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function removeFromCart(item) {
      return `That item is not in your cart.`
  }
  else (cart.splice(cart.item, 1,)
-  return cart
+  return cart;
 }
 
 function placeOrder(cardNumber) {
